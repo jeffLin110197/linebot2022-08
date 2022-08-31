e152ba396380e830dd7a93a9c7ffed70dd5affd2
jeffLin110197 <bank110197@gmail.com>
2022-08-31 16:00:49 +0800

    add def getNameEmojiMessage()

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ def sendTextMessageToMe():
 def getNameEmojiMessage():
     lookUpStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     productId = "5ac21a8c040ab15980c9b43f"
-    name = "Frank"
+    name = "Jeff"
     message = dict()
     message["type"] = "text"
     message["text"] = "".join("$" for r in range(len(name)))
