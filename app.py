dac852a0295ec07dafec198a04c992691435cf36
jeffLin110197 <bank110197@gmail.com>
2022-08-31 15:24:29 +0800

    add def getPlayStickerMessage()

--- app.py
+++ app.py
@@ -202,6 +202,9 @@ def getCallCarMessage(data):
 
 def getPlayStickerMessage():
     message = dict()
+    message["type"] = "sticker"
+    message["packageId"] = "446"
+    message["stickerId"] = "1988"
     return message
 
 
