87e3d27aec7a6818ad1696c3764370fa7f8dc692
jeffLin110197 <bank110197@gmail.com>
2022-08-31 18:06:48 +0800

    add def getCallCarMessage(data)

--- app.py
+++ app.py
@@ -241,20 +241,20 @@ def getLocationConfirmMessage(title, latitude, longitude):
 
 def getCallCarMessage(data):
     message = {
-        "type": "template",
-        "altText": "this is a template",
-        "template": {
-            "type": "buttons",
-            "text": f"請選擇至 {data['title']} 預約叫車時間",
-            "actions" : [
-                {
-                    "type": "datetimepicker",
-                    "label": "預約",
-                    "data": json.dumps(data),
-                    "mode": "datatime"
-                }
-            ]            
-        }
+      "type": "template",
+      "altText": "this is a template",
+      "template": {
+          "type": "buttons",
+          "text": f"請選擇至 {data['title']} 預約叫車時間",
+          "actions": [
+              {
+               "type": "datetimepicker",
+               "label": "預約",
+               "data": json.dumps(data),
+               "mode": "datetime"
+               }
+          ]
+      }
     }
     return message
 
